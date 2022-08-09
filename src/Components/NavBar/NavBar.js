52e3dd94dacb78be02a1185b119effb4149e15a8
Ahmed Hassan <ahmed.hassan@Aru.edu.eg>
2022-08-10 01:59:32 +0200

    update navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,46 +1,76 @@
-import React from 'react';
+import {React,useState} from 'react';
 // Bootstrap Components
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Dropdown from 'react-bootstrap/Dropdown';
 import Container from 'react-bootstrap/Container';
 import { AiOutlineSetting } from "react-icons/ai";
 import { AiTwotoneBank } from "react-icons/ai";
-// End Of Bootstrap Components
+import { FaUserCircle } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 
+// End Of Bootstrap Components
 import classes from './NavBar.module.scss';
 
 const NavBar = () => {
+    const [arrow,setArrow]= useState('down')
+    const arrowHandler=()=>{
+        if(arrow == 'down'){
+            setArrow('up');
+        }else{
+            setArrow('down');
+        }
+    }
+
     return (
 
         <Navbar expand="lg" dir='rtl' className={`${classes.navbar}`}>
             <Container>
                 <Navbar href="" className={`justify-content-start ${classes.navbar_link}`}>Logo</Navbar>
 
-                <Navbar.Toggle className={`${classes.navbar_link}`} />
+                <Navbar.Toggle/>
 
                 <Navbar.Collapse className={`justify-content-start ${classes.navbar_link}`}>
                     <Nav>
-                        <NavDropdown title={
-                            <span className={`${classes.navbar_link}`}> <AiTwotoneBank className={`${classes.nav_icon}`} />الاصول</span>
-                        } className={`${classes.dropdown_link}`}>
+                        <NavDropdown className={`${classes.dropdown}`} title={
+                            <span className={`${classes.navbar_link}`}>
+                                <AiOutlineSetting className={`${classes.nav_icon}`} /> 
+                                التصنيع
+                                {
+                                    arrow == 'down' ? <IoIosArrowDown className={`${classes.nav_icon_down}`} /> : <IoIosArrowUp className={`${classes.nav_icon_down}`} /> 
+                                }
+                            </span>
+                        } onClick={arrowHandler}>
                             <NavDropdown.Item href="">1</NavDropdown.Item>
                             <NavDropdown.Item href="">2</NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title={
-                            <span className={`${classes.navbar_link}`}><AiOutlineSetting className={`${classes.nav_icon}`} /> التصنيع</span>
-                        }>
+
+                        <NavDropdown className={`${classes.dropdown}`} title={
+                            <span className={`${classes.navbar_link}`}> 
+                                <AiTwotoneBank className={`${classes.nav_icon}`} />
+                                الاصول
+                                {
+                                    arrow == 'down' ? <IoIosArrowDown className={`${classes.nav_icon_down}`} /> : <IoIosArrowUp className={`${classes.nav_icon_down}`} /> 
+                                } 
+                            </span>
+                        } onClick={arrowHandler}>
                             <NavDropdown.Item href="">1</NavDropdown.Item>
                             <NavDropdown.Item href="">2</NavDropdown.Item>
                         </NavDropdown>
+                        
                     </Nav>
                 </Navbar.Collapse>
                 
                 <Navbar.Collapse className={`justify-content-end ${classes.navbar_link}`}>
-                    <Navbar href="" className={` ${classes.navbar_link}`}>اسم المستخدم</Navbar>
+                    <Navbar  href="" className={` ${classes.navbar_link} ${classes.dropdown}`}>
+                        <FaUserCircle className={`${classes.nav_icon} ${classes.nav_icon_user}`} />  اسم المستخدم 
+                    </Navbar>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
+      
     );
 };
 
